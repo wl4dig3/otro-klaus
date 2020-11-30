c3dbd2f27b84d7bb1775ef5d9d40c5388449f3b0
Wladimir Guevara <wladige@gmail.com>
2020-11-30 20:44:55 -0300

    agregando wait de 3seg para dar tiempo a la respuesta de la consulta a firestore

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,12 +4,13 @@ describe('My First Test', () => {
   it('Probando crear usuario en vista Login y editando el crud', () => {
     cy.visit('/');
     cy.get('#entrar').click();
-    cy.get('#input-1').type('test15@tata.com');
+    cy.get('#input-1').type('test4@tata.com');
     cy.get('#input-2').type('12341234');
     cy.get('#crear_usuario').click();
+    cy.wait(3000);
     cy.get('#botoneditar').click();
-    cy.get('#campocod').type('R-016');
-    cy.get('#camponombre').type(' nuevo nombre desde testin para editar');
+    cy.get('#campocod').type('R-013');
+    cy.get('#camponombre').type(' nuevo nombre desde testin para editar'); // probar editar producto
     cy.get('#campostock').type(990);
     cy.get('#campoprecio').type(21990);
     cy.get('#campodescripcion').type('Lo mas nuevo');
@@ -25,13 +26,14 @@ describe('My First Test', () => {
     cy.get('#input-1').type('quesito@game.com');
     cy.get('#input-2').type('12341234');
     cy.get('#boton-login').click();
-    cy.get('#input-grup-1').type('R-015');
+    cy.get('#input-grup-1').type('R-016');
     cy.get('#input-grup-2').type('Test');
     cy.get('#input-grup-3').type(5);
     cy.get('#input-grup-4').type(29900);
     cy.get('#input-grup-5').type('Agregando producto desde testing');
 
     cy.get('#agregar-productos').click(); // boton para agregar el nuevo item o producto
+    cy.wait(3000);
     cy.get('#botonsalir').click(); // fin de prueba
 
 
